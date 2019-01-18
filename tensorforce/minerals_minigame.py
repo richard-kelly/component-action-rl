2972bc351283b8f88c51f42443da2271b8b945bf
Richard Kelly <z24rmk@mun.ca>
2019-01-18 13:38:52 -0330

    changed network and agent settings. can't figure out how to specify what output part(s) of network go to which actions

--- tensorforce/minerals_minigame.py
+++ tensorforce/minerals_minigame.py
@@ -9,38 +9,11 @@ from pysc2.lib import actions
 
 FUNCTIONS = actions.FUNCTIONS
 
+# TODO: JSON config file here
+model_dir = 'models/test1'
+
 # to use:
 # python -m pysc2.bin.agent --map CollectMineralShards --agent minerals_minigame.TestAgent
-
-'''
-network_spec = [
-    [
-        dict(type='input', inputs=['screen']),
-        dict(type='embedding', indices=2, size=64),
-        dict(type='flatten'),
-        dict(type='dense', size=64),
-        dict(type='output', output='screen_net')
-    ],
-    [
-
-        dict(type='input', inputs=['screen_net']),
-        dict(type='dense', size=64),
-        dict(type='output', output='output')
-    ]
-]
-'''
-'''[
-        dict(type='input', inputs=['minimap']),
-        dict(type='dense', size=64),
-        dict(type='output', output='minimap_net')
-    ],
-    [
-        dict(type='input', inputs=['player', 'game_loop', 'score_cumulative', 'available_actions', 'single_select', 'control_groups']),
-        dict(type='dense', size=64),
-        dict(type='output', output='non_spatial_net')
-],'''
-# dict(type='input', inputs=['screen_net', 'minimap_net', 'non_spatial_net']),
-# modify obs_spec to fit right format
 
 '''
 actions (spec, or dict of specs): Actions specification, with the following attributes (required):
@@ -58,16 +31,29 @@ class TestAgent(base_agent.BaseAgent):
         self.validLastAction = False
 
     def preprocess_state(self, obs):
+        state = dict()
+
         # just using some features that should give minimum info needed for collect minerals minigame
         player_relative = utils.one_hot_encode_int_array(obs.observation['feature_screen'].player_relative, 5)
         # everything is 3 dimensional before concat
         selected = np.expand_dims(obs.observation['feature_screen'].selected, axis=0)
-        return np.concatenate((player_relative, selected), axis=0)
+        state['screen'] = np.concatenate((player_relative, selected), axis=0)
+
+        obs.observation['available_actions']
+        avail_actions = []
+        for i in range(len(FUNCTIONS)):
+            if i in obs.observation['available_actions']:
+                avail_actions.append(1)
+            else:
+                avail_actions.append(0)
+        state['available_actions'] = np.array(avail_actions)
+
+        return state
 
     def getActionSpec(self, action_spec):
-        # all_actions = {'function': dict(type='int', num_actions=len(action_spec.functions))}
+        all_actions = {'function': dict(type='int', num_actions=len(action_spec.functions))}
         # version with limited actions to speed up learning of simple minigame
-        all_actions = {'function': dict(type='int', num_actions=21)}
+        # all_actions = {'function': dict(type='int', num_actions=21)}
         for argument_type in action_spec.types:
             # the only arguments with a shape that isn't (1) are the screen and minimap ones,
             # so we're assuming the screen/minimap dimensions are square here
@@ -76,52 +62,61 @@ class TestAgent(base_agent.BaseAgent):
         return all_actions
 
     def getStateSpec(self, obs_spec):
-        states = dict(type='float', shape=(6, 84, 84))
+        states = dict(
+            screen=dict(type='float', shape=(6, 84, 84)),
+            available_actions=dict(type='float', shape=(len(FUNCTIONS)))
+        )
         return states
 
     def setup(self, obs_spec, action_spec):
         super().setup(obs_spec, action_spec)
 
         network_spec = [
-            dict(
-                size=32,
-                type='conv2d',
-                window=5,
-                stride=1,
-                padding='SAME',
-                bias=True,
-                activation='relu',
-                l2_regularization=0.0,
-                l1_regularization=0.0,
-            ),
-            dict(
-                type='pool2d',
-                pooling_type='max',
-                window=2,
-                stride=2,
-                padding='SAME',
-            ),
-            dict(
-                size=32,
-                type='conv2d',
-                window=3,
-                stride=1,
-                padding='SAME',
-                bias=True,
-                activation='relu',
-                l2_regularization=0.0,
-                l1_regularization=0.0,
-            ),
-            dict(
-                type='pool2d',
-                pooling_type='average',
-                window=2,
-                stride=2,
-                padding='SAME',
-            ),
-            dict(type='flatten'),
-            dict(type='dense', size=512),
-            dict(type='dense', size=512)
+            [
+                dict(type='input', names=['screen']),
+                dict(
+                    size=32,
+                    type='conv2d',
+                    window=5,
+                    stride=1,
+                    padding='SAME',
+                    bias=True,
+                    activation='relu',
+                    l2_regularization=0.0,
+                    l1_regularization=0.0,
+                ),
+                dict(
+                    size=64,
+                    type='conv2d',
+                    window=3,
+                    stride=1,
+                    padding='SAME',
+                    bias=True,
+                    activation='relu',
+                    l2_regularization=0.0,
+                    l1_regularization=0.0,
+                ),
+                dict(
+                    size=64,
+                    type='conv2d',
+                    window=3,
+                    stride=1,
+                    padding='SAME',
+                    bias=True,
+                    activation='relu',
+                    l2_regularization=0.0,
+                    l1_regularization=0.0,
+                ),
+                dict(type='flatten'),
+                dict(type='dense', size=512),
+                dict(type='output', name='screen_net')
+            ],
+            [
+                dict(type='input', names=['screen_net', 'available_actions']),
+                dict(type='flatten'),
+                dict(type='dense', size=512),
+                dict(type='dense', size=512)
+            ]
         ]
 
         self.tfAgent = DQNAgent(
@@ -129,12 +124,13 @@ class TestAgent(base_agent.BaseAgent):
             actions=self.getActionSpec(action_spec),
             network=network_spec,
             batching_capacity=10,
+            discount=0.99,
             saver=dict(
-                directory='models/test3',
+                directory=model_dir,
                 seconds=6000
             ),
             summarizer=dict(
-                directory='models/test3',
+                directory=model_dir,
                 seconds=30,
                 labels=[
                     'configuration',
@@ -145,38 +141,25 @@ class TestAgent(base_agent.BaseAgent):
             ),
             update_mode=dict(
                 unit='timesteps',
-                batch_size=8,
+                batch_size=64,
                 frequency=8
             ),
             memory=dict(
                 type='replay',
                 include_next_states=True,
-                capacity=100
+                capacity=1000
             ),
-            # memory=dict(
-            #     type='prioritized_replay',
-            #     include_next_states=True,
-            #     buffer_size=50,
-            #     capacity=100
-            # ),
             optimizer=dict(
                 type='adam',
-                learning_rate=10e-3
+                learning_rate=1e-3
             ),
-            # actions_exploration=dict(
-            #     type="epsilon_decay",
-            #     initial_epsilon=1.0,
-            #     final_epsilon=0.1,
-            #     timesteps=100000
-            # ),
             actions_exploration=dict(
                 type="epsilon_anneal",
-                initial_epsilon=1.0,
+                initial_epsilon=0.9,
                 final_epsilon=0.1,
                 timesteps=1000000
             )
         )
-
 
     def getActionFunction(self, obs, action):
         id = action['function']
