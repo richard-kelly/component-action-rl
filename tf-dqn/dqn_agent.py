710c85f48e47cd06fe46009f6d81be3264560d68
Richard Kelly <z24rmk@mun.ca>
2019-02-19 15:55:01 -0330

    logging replay duration

--- tf-dqn/dqn_agent.py
+++ tf-dqn/dqn_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import random
 import json
+import time
 
 from network import Network
 from memory import Memory
@@ -18,6 +19,15 @@ class DQNAgent:
         self._last_state = None
         self._last_reward = None
         self._last_action = None
+
+        self._times = dict(
+            sample=0,
+            predict_batch_states=0,
+            predict_batch_next_states=0,
+            calculating_y=0,
+            train_batch=0
+        )
+        self._time_count = 0
 
         # initialize epsilon
         self._epsilon = config['initial_epsilon']
@@ -59,7 +69,7 @@ class DQNAgent:
         self._steps += 1
 
         # do a batch of learning every "update_frequency" steps
-        if self._steps % config['update_frequency'] == 0:
+        if self._steps % config['update_frequency'] == 0 and self._memory.get_size() >= config['batch_size']:
             self._replay()
 
         # save checkpoint if needed
@@ -108,16 +118,28 @@ class DQNAgent:
             )
 
     def _replay(self):
+
+        last_time = time.time()
+
         # states stored in memory as tuple (state, action, reward, next_state)
         # next_state=None if state is terminal
         states, actions, rewards, next_states, is_terminal = self._memory.sample(config['batch_size'])
 
+        self._times['sample'] += time.time() - last_time
+        last_time = time.time()
+
         # predict Q(s,a) given the batch of states
         q_s_a = self._network.predict_batch(states, self._sess)
+
+        self._times['predict_batch_states'] += time.time() - last_time
+        last_time = time.time()
 
         # TODO: this should be a different target network updated periodically
         # predict Q(s',a') - so that we can do gamma * max(Q(s'a')) below
         q_s_a_target = self._network.predict_batch(next_states, self._sess)
+
+        self._times['predict_batch_next_states'] += time.time() - last_time
+        last_time = time.time()
 
         for i in range(rewards.shape[0]):
             # update the q value for action
@@ -147,7 +169,19 @@ class DQNAgent:
                 # for key in action.keys():
                 #     q_s_a[key][i][action[key]] = reward + config['discount'] * np.amax(q_s_a_target[key][i])
 
+        self._times['calculating_y'] += time.time() - last_time
+        last_time = time.time()
+
         self._network.train_batch(self._sess, states, q_s_a)
+
+        self._times['train_batch'] += time.time() - last_time
+        self._time_count += 1
+        if self._time_count == config['log_frequency']:
+            print("average ms for", config['log_frequency'], "replays:")
+            for k, v in self._times.items():
+                print("%25s:%.2f" % (k, v / (config['log_frequency'] / 1000)))
+                self._times[k] = 0
+            self._time_count = 0
 
     def __del__(self):
         self._sess.close()
