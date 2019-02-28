667b51e34e8e32749b5c374ee6c5aa9469eb77f1
Rick Kelly <z24rmk@mun.ca>
2019-02-28 10:22:08 -0330

    Better tf name scope usage for tensorboard graph

--- tf-dqn/network.py
+++ tf-dqn/network.py
@@ -33,7 +33,7 @@ class Network:
                 filters=16,
                 kernel_size=5,
                 padding='same',
-                name=scope_name + '_conv1_spatial'
+                name='conv1_spatial'
             )
 
             conv2_spatial = tf.layers.conv2d(
@@ -41,7 +41,7 @@ class Network:
                 filters=32,
                 kernel_size=3,
                 padding='same',
-                name=scope_name + '_conv2_spatial'
+                name='conv2_spatial'
             )
 
             max_pool = tf.layers.max_pooling2d(
@@ -49,20 +49,20 @@ class Network:
                 pool_size=3,
                 strides=3,
                 padding='valid',
-                name=scope_name + '_max_pool'
+                name='max_pool'
             )
 
             # MUST flatten conv or pooling layers before sending to dense layer
             non_spatial_flat = tf.reshape(
                 max_pool,
                 shape=[-1, int(self._screen_size * self._screen_size / 9 * 32)],
-                name=scope_name + '_conv2_spatial_flat'
+                name='conv2_spatial_flat'
             )
             fc_non_spatial = tf.layers.dense(
                 non_spatial_flat,
                 512,
                 activation=tf.nn.relu,
-                name=scope_name + '_fc_spatial'
+                name='fc_spatial'
             )
 
             spatial_policy_1 = tf.layers.conv2d(
@@ -70,7 +70,7 @@ class Network:
                 filters=1,
                 kernel_size=1,
                 padding='same',
-                name=scope_name + '_spatial_policy_1'
+                name='spatial_policy_1'
             )
 
             spatial_policy_2 = tf.layers.conv2d(
@@ -78,31 +78,32 @@ class Network:
                 filters=1,
                 kernel_size=1,
                 padding='same',
-                name=scope_name + '_spatial_policy_2'
+                name='spatial_policy_2'
             )
 
             logits = dict(
-                function=tf.layers.dense(fc_non_spatial, 4, name=scope_name + '_function'),
-                screen=tf.reshape(spatial_policy_1, [-1, self._screen_size * self._screen_size], name=scope_name + '_screen_policy'),
-                screen2=tf.reshape(spatial_policy_2, [-1, self._screen_size * self._screen_size], name=scope_name + '_screen2_policy'),
-                select_point_act=tf.layers.dense(fc_non_spatial, 4, name=scope_name + '_select_point_act'),
-                select_add=tf.layers.dense(fc_non_spatial, 2, name=scope_name + '_select_add'),
-                queued=tf.layers.dense(fc_non_spatial, 2, name=scope_name + '_queued')
+                function=tf.layers.dense(fc_non_spatial, 4, name='function'),
+                screen=tf.reshape(spatial_policy_1, [-1, self._screen_size * self._screen_size], name='screen_policy'),
+                screen2=tf.reshape(spatial_policy_2, [-1, self._screen_size * self._screen_size], name='screen2_policy'),
+                select_point_act=tf.layers.dense(fc_non_spatial, 4, name='select_point_act'),
+                select_add=tf.layers.dense(fc_non_spatial, 2, name='select_add'),
+                queued=tf.layers.dense(fc_non_spatial, 2, name='queued')
             )
 
-            return logits
+        return logits
 
     def _define_model(self):
 
         self._states = tf.placeholder(shape=[None, self._screen_size, self._screen_size, 5], dtype=tf.float32, name='states_placeholder')
-        self._actions = dict(
-            function=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_function_placeholder'),
-            screen=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_screen_placeholder'),
-            screen2=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_screen2_placeholder'),
-            select_point_act=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_select_point_act_placeholder'),
-            select_add=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_select_add_placeholder'),
-            queued=tf.placeholder(shape=[None, ], dtype=tf.int32, name='action_queued_placeholder')
-        )
+        with tf.variable_scope('action_placeholders'):
+            self._actions = dict(
+                function=tf.placeholder(shape=[None, ], dtype=tf.int32, name='function'),
+                screen=tf.placeholder(shape=[None, ], dtype=tf.int32, name='screen'),
+                screen2=tf.placeholder(shape=[None, ], dtype=tf.int32, name='screen2'),
+                select_point_act=tf.placeholder(shape=[None, ], dtype=tf.int32, name='select_point_act'),
+                select_add=tf.placeholder(shape=[None, ], dtype=tf.int32, name='select_add'),
+                queued=tf.placeholder(shape=[None, ], dtype=tf.int32, name='queued')
+            )
         self._rewards = tf.placeholder(shape=[None, ], dtype=tf.float32, name='reward_placeholder')
         self._next_states = tf.placeholder(shape=[None, self._screen_size, self._screen_size, 5], dtype=tf.float32, name='next_states_placeholder')
         self._not_terminal = tf.placeholder(shape=[None, ], dtype=tf.float32, name='not_terminal_placeholder')
@@ -113,36 +114,39 @@ class Network:
         self._q_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Q_primary")
         self._q_target_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Q_target")
 
-        action_one_hot = dict(
-            function=tf.one_hot(self._actions['function'], 4, 1.0, 0.0, name='function_one_hot'),
-            screen=tf.one_hot(self._actions['screen'], self._screen_size * self._screen_size, 1.0, 0.0, name='screen_one_hot'),
-            screen2=tf.one_hot(self._actions['screen2'], self._screen_size * self._screen_size, 1.0, 0.0, name='screen2_one_hot'),
-            select_point_act=tf.one_hot(self._actions['select_point_act'], 4, 1.0, 0.0, name='select_point_act_one_hot'),
-            select_add=tf.one_hot(self._actions['select_add'], 2, 1.0, 0.0, name='select_add_one_hot'),
-            queued=tf.one_hot(self._actions['queued'], 2, 1.0, 0.0, name='queued_one_hot')
-        )
-        # The prediction by the primary Q network for the actual actions
-        pred = {}
-        for name, q_vals in self._q.items():
-            pred[name] = tf.reduce_sum(q_vals * action_one_hot[name], reduction_indices=-1, name='q_acted_' + name)
+        with tf.variable_scope('action_one_hot'):
+            action_one_hot = dict(
+                function=tf.one_hot(self._actions['function'], 4, 1.0, 0.0, name='function'),
+                screen=tf.one_hot(self._actions['screen'], self._screen_size * self._screen_size, 1.0, 0.0, name='screen'),
+                screen2=tf.one_hot(self._actions['screen2'], self._screen_size * self._screen_size, 1.0, 0.0, name='screen2'),
+                select_point_act=tf.one_hot(self._actions['select_point_act'], 4, 1.0, 0.0, name='select_point_act'),
+                select_add=tf.one_hot(self._actions['select_add'], 2, 1.0, 0.0, name='select_add'),
+                queued=tf.one_hot(self._actions['queued'], 2, 1.0, 0.0, name='queued')
+            )
 
-        # The optimization target
-        max_q_next_by_target = {}
-        for name, q_vals in self._q_target.items():
-            max_q_next_by_target[name] = tf.reduce_max(q_vals, axis=-1)
+        with tf.variable_scope('prediction'):
+            # The prediction by the primary Q network for the actual actions
+            pred = {}
+            for name, q_vals in self._q.items():
+                pred[name] = tf.reduce_sum(q_vals * action_one_hot[name], reduction_indices=-1, name=name)
 
-        y = {}
-        for name, max_q_next in max_q_next_by_target.items():
-            y[name] = self._rewards + self._not_terminal * self._discount * max_q_next
+        with tf.variable_scope('optimization_target'):
+            # The optimization target
+            max_q_next_by_target = {}
+            for name, q_vals in self._q_target.items():
+                max_q_next_by_target[name] = tf.reduce_max(q_vals, axis=-1, name=name)
 
-        losses = []
-        for name in y.keys():
-            print(name)
-            print(pred[name].shape)
-            print(y[name].shape)
-            losses.append(tf.losses.mean_squared_error(pred[name], tf.stop_gradient(y[name])))
+        with tf.variable_scope('y'):
+            y = {}
+            for name, max_q_next in max_q_next_by_target.items():
+                y[name] = self._rewards + self._not_terminal * self._discount * max_q_next
 
-        loss = tf.add_n(losses, name='losses_sum')
+        with tf.variable_scope('losses'):
+            losses = []
+            for name in y.keys():
+                losses.append(tf.losses.mean_squared_error(pred[name], tf.stop_gradient(y[name])))
+
+            loss = tf.add_n(losses, name='losses_sum')
 
         self._optimizer = tf.train.AdamOptimizer(learning_rate=self._learning_rate).minimize(loss)
 
