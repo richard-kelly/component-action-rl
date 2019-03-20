7dc8b1eb18f417535d69c02478ddfe680f7c3ca2
Rick Kelly <z24rmk@mun.ca>
2019-03-20 15:53:52 -0230

    wip runner batch

--- tf-dqn/dqn_agent.py
+++ tf-dqn/dqn_agent.py
@@ -2,18 +2,14 @@ import tensorflow as tf
 import numpy as np
 import math
 import random
-import json
 import time
 
 from network import Network
 from memory import Memory
 
-with open('config.json', 'r') as fp:
-    config = json.load(fp=fp)
-
 
 class DQNAgent:
-    def __init__(self, restore):
+    def __init__(self, sess, config, restore):
         self._steps = 0
         self._episodes = 0
         self._episode_score = 0
@@ -22,6 +18,7 @@ class DQNAgent:
         self._last_reward = None
         self._last_action = None
         self._sample_action = None
+        self._config = config
 
         self._times = dict(
             sample=0,
@@ -30,40 +27,40 @@ class DQNAgent:
         self._time_count = 0
 
         # initialize epsilon
-        if not config['run_model_no_training']:
-            self._epsilon = config['initial_epsilon']
+        if not self._config['run_model_no_training']:
+            self._epsilon = self._config['initial_epsilon']
         else:
             self._epsilon = 0.0
-        if config['decay_type'] == "exponential":
-            self._decay = math.exp(math.log(config['final_epsilon'] / config['initial_epsilon'])/config['decay_steps'])
-        elif config['decay_type'] == "linear":
-            self._decay = (config['initial_epsilon'] - config['final_epsilon']) / config['decay_steps']
+        if self._config['decay_type'] == "exponential":
+            self._decay = math.exp(math.log(self._config['final_epsilon'] / self._config['initial_epsilon'])/self._config['decay_steps'])
+        elif self._config['decay_type'] == "linear":
+            self._decay = (self._config['initial_epsilon'] - self._config['final_epsilon']) / self._config['decay_steps']
         else:
             self._decay = 0.0
 
-        self._memory = Memory(config['memory_size'])
+        self._memory = Memory(self._config['memory_size'])
         self._network = Network(
-            config['learning_rate'],
-            config['discount'],
-            config['model_checkpoint_max'],
-            config['model_checkpoint_every_n_hours'],
-            config['reg_type'],
-            config['reg_scale'],
-            config['env']
+            self._config['learning_rate'],
+            self._config['discount'],
+            self._config['model_checkpoint_max'],
+            self._config['model_checkpoint_every_n_hours'],
+            self._config['reg_type'],
+            self._config['reg_scale'],
+            self._config['env']
         )
-        self._sess = tf.Session()
-        self._writer = tf.summary.FileWriter(config['model_dir'], self._sess.graph)
+        self._sess = sess
+        self._writer = tf.summary.FileWriter(self._config['model_dir'], self._sess.graph)
 
         if restore:
             try:
-                checkpoint = tf.train.get_checkpoint_state(config['model_dir'])
+                checkpoint = tf.train.get_checkpoint_state(self._config['model_dir'])
                 self._network.saver.restore(self._sess, checkpoint.model_checkpoint_path)
                 self._steps = int(checkpoint.model_checkpoint_path.split('-')[-1])
                 # this makes sure tensorboard deletes any "future" events logged after the checkpoint
                 self._writer.add_session_log(tf.SessionLog(status=tf.SessionLog.START), global_step=self._steps)
 
                 # adjust epsilon for current step
-                self._update_epsilon(min(config['decay_steps'], self._steps))
+                self._update_epsilon(min(self._config['decay_steps'], self._steps))
                 print("Model restored at step: ", self._steps, ", epsilon: ", self._epsilon)
 
             except (ValueError, AttributeError):
@@ -83,7 +80,7 @@ class DQNAgent:
             self._writer.add_summary(summary, self._steps)
             self._episode_score = 0
 
-        if not config['run_model_no_training']:
+        if not self._config['run_model_no_training']:
             self._last_reward = reward
             # at end of episode store memory sample with None for next state
             # set last_state to None so that on next act() we know it is beginning of episode
@@ -96,44 +93,44 @@ class DQNAgent:
         action = self._choose_action(state, available_actions)
         self._steps += 1
 
-        if not config['run_model_no_training']:
+        if not self._config['run_model_no_training']:
             if self._last_state is not None:
                 self._memory.add_sample(self._last_state, self._last_action, self._last_reward, state, False)
 
             # update target network parameters occasionally
-            if self._steps % config['target_update_frequency'] == 0:
+            if self._steps % self._config['target_update_frequency'] == 0:
                 print('updating target network')
                 self._network.update_target_q_net(self._sess)
 
             # do a batch of learning every "update_frequency" steps
-            if self._steps % config['update_frequency'] == 0 and self._memory_start_size_reached:
+            if self._steps % self._config['update_frequency'] == 0 and self._memory_start_size_reached:
                 # only start training once memory has reached minimum size
                 self._replay()
 
             # save checkpoint if needed
-            if self._steps % config['model_checkpoint_frequency'] == 0:
+            if self._steps % self._config['model_checkpoint_frequency'] == 0:
                 save_path = self._network.saver.save(
                     sess=self._sess,
-                    save_path=config['model_dir'] + '/model.ckpt',
+                    save_path=self._config['model_dir'] + '/model.ckpt',
                     global_step=self._steps
                 )
                 print("Model saved in path: %s" % save_path)
 
-            if self._steps <= config['decay_steps'] + config['memory_burn_in'] and self._memory_start_size_reached:
+            if self._steps <= self._config['decay_steps'] + self._config['memory_burn_in'] and self._memory_start_size_reached:
                 # only start changing epsilon once memory has reached minimum size
                 self._update_epsilon()
 
             self._last_state = state
             self._last_action = action
 
-            self._memory_start_size_reached = self._memory.get_size() >= config['memory_burn_in']
+            self._memory_start_size_reached = self._memory.get_size() >= self._config['memory_burn_in']
 
         return action
 
     def _update_epsilon(self, steps=1):
-        if config['decay_type'] == "exponential":
+        if self._config['decay_type'] == "exponential":
             self._epsilon = self._epsilon * (self._decay ** steps)
-        elif config['decay_type'] == "linear":
+        elif self._config['decay_type'] == "linear":
             self._epsilon = self._epsilon - (self._decay * steps)
 
     def _choose_action(self, state, available_actions):
@@ -144,8 +141,8 @@ class DQNAgent:
                 _, self._sample_action = self._network.predict_one(self._sess, state)
             for name, q_values in self._sample_action.items():
                 if available_actions and name in available_actions:
-                    if name in config['env']['action_list']:
-                        valid = np.in1d(config['env']['action_list'][name], available_actions[name])
+                    if name in self._config['env']['action_list']:
+                        valid = np.in1d(self._config['env']['action_list'][name], available_actions[name])
                         options = np.nonzero(valid)[0]
                         action[name] = np.random.choice(options)
                     else:
@@ -158,8 +155,8 @@ class DQNAgent:
             for name, q_values in pred.items():
                 if available_actions and name in available_actions:
                     q_values = q_values.flatten()
-                    if name in config['env']['action_list']:
-                        valid = np.in1d(config['env']['action_list'][name], available_actions[name])
+                    if name in self._config['env']['action_list']:
+                        valid = np.in1d(self._config['env']['action_list'][name], available_actions[name])
                     else:
                         valid = np.zeros(q_values.shape, dtype=np.int32)
                         valid[available_actions[name]] = 1
@@ -173,7 +170,7 @@ class DQNAgent:
 
         # states stored in memory as tuple (state, action, reward, next_state)
         # next_state=None if state is terminal
-        states, actions, rewards, next_states, is_terminal = self._memory.sample(config['batch_size'])
+        states, actions, rewards, next_states, is_terminal = self._memory.sample(self._config['batch_size'])
 
         self._times['sample'] += time.time() - last_time
         last_time = time.time()
@@ -183,12 +180,9 @@ class DQNAgent:
 
         self._times['train_batch'] += time.time() - last_time
         self._time_count += 1
-        if config['debug_logging'] and self._time_count == config['log_frequency']:
-            print("average ms for", config['log_frequency'], "replays:")
+        if self._config['debug_logging'] and self._time_count == self._config['log_frequency']:
+            print("average ms for", self._config['log_frequency'], "replays:")
             for k, v in self._times.items():
-                print("%25s:%.2f" % (k, v / (config['log_frequency'] / 1000)))
+                print("%25s:%.2f" % (k, v / (self._config['log_frequency'] / 1000)))
                 self._times[k] = 0
             self._time_count = 0
-
-    def __del__(self):
-        self._sess.close()
--- tf-dqn/runner.py
+++ tf-dqn/runner.py
@@ -3,13 +3,14 @@ import json
 import os
 import shutil
 import sys
+import tensorflow as tf
 
 from absl import flags
-
 from pysc2.lib import actions
+from pysc2.lib import features
 from pysc2.env.environment import StepType
 from pysc2.env import sc2_env
-from pysc2.lib import features
+
 
 from dqn_agent import DQNAgent
 import utils
@@ -18,27 +19,11 @@ import utils
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
-FUNCTIONS = actions.FUNCTIONS
 
-# load configuration
-with open('config.json', 'r') as fp:
-    config = json.load(fp=fp)
-
-# masking the actions functions so only these actions can be taken
-relevant_actions = config['env']['action_list']['function']
-
-# save a copy of the configuration files being used for a run in the run's folder (first time only)
-restore = True
-if not os.path.exists(config['model_dir']):
-    restore = False
-    os.makedirs(config['model_dir'])
-    shutil.copy2('config.json', config['model_dir'])
-
-
-def get_action_function(obs, action):
+def get_action_function(obs, action, actions_in_use, screen_size):
     # id = action['function']
     # Masked actions instead
-    id = relevant_actions[action['function']]
+    id = actions_in_use[action['function']]
 
     if id not in obs.observation['available_actions']:
         # no_op - should not happen
@@ -46,8 +31,9 @@ def get_action_function(obs, action):
         return actions.FunctionCall(0, [])
 
     args = []
-    for i in range(len(FUNCTIONS[id].args)):
-        name = FUNCTIONS[id].args[i].name
+    pysc2_funcs = actions.FUNCTIONS
+    for i in range(len(pysc2_funcs[id].args)):
+        name = pysc2_funcs[id].args[i].name
         # special case of func id 3, select_rect, used only if 'screen2' isn't output by network
         # just select a rectangle around the point given by 'screen'
         if id == 3 and (name == 'screen' or name == 'screen2') and 'screen2' not in action:
@@ -56,14 +42,13 @@ def get_action_function(obs, action):
             if name == 'screen':
                 args.append([max(x - half_rect, 0), max(y - half_rect, 0)])
             elif name == 'screen2':
-                s = config['env']['screen_size']
-                args.append([min(x + half_rect, s - 1), min(y + half_rect, s - 1)])
+                args.append([min(x + half_rect, screen_size - 1), min(y + half_rect, screen_size - 1)])
         else:
             if name == 'screen':
-                x, y = get_screen_coords(action['screen'])
+                x, y = get_screen_coords(action['screen'], screen_size)
                 args.append([x, y])
             elif name == 'screen2':
-                x, y = get_screen_coords(action['screen2'])
+                x, y = get_screen_coords(action['screen2'], screen_size)
                 args.append([x, y])
             elif name not in action:
                 # if network doesn't supply argument, uses first choice, which is usually default no modifier action
@@ -73,9 +58,9 @@ def get_action_function(obs, action):
     return actions.FunctionCall(id, args)
 
 
-def get_screen_coords(val):
-    y = val // config['env']['screen_size']
-    x = val % config['env']['screen_size']
+def get_screen_coords(val, screen_size):
+    y = val // screen_size
+    x = val % screen_size
     return x, y
 
 
@@ -91,42 +76,63 @@ def preprocess_state(obs):
     return state
 
 
-def main():
+def run_one_env(config, rename_if_duplicate):
+    # save a copy of the configuration file being used for a run in the run's folder (first time only)
+    restore = True
+    if not os.path.exists(config['model_dir']):
+        restore = False
+        os.makedirs(config['model_dir'])
+        shutil.copy2('config.json', config['model_dir'])
+
     with sc2_env.SC2Env(
-        map_name=config['env']['map_name'],
-        players=[sc2_env.Agent(sc2_env.Race['random'], None)],
-        agent_interface_format=features.AgentInterfaceFormat(
-            feature_dimensions=features.Dimensions(config['env']['screen_size'], config['env']['minimap_size'])
-        ),
-        visualize=config['env']['visualize'],
-        step_mul=config['env']['step_mul']
+            map_name=config['env']['map_name'],
+            players=[sc2_env.Agent(sc2_env.Race['random'], None)],
+            agent_interface_format=features.AgentInterfaceFormat(
+                feature_dimensions=features.Dimensions(config['env']['screen_size'], config['env']['minimap_size'])
+            ),
+            visualize=config['env']['visualize'],
+            step_mul=config['env']['step_mul']
     ) as env:
-        rl_agent = DQNAgent(restore)
-        obs = env.reset()[0]
-        step = 0
-        episode = 1
-        episode_reward = 0
-        while step <= 1000000:
-            step += 1
-            state = preprocess_state(obs)
-            available_actions = dict(
-                function=obs.observation['available_actions']
-            )
+        with tf.Session() as sess:
+            rl_agent = DQNAgent(sess, config, restore)
+            # observations from the env are tuples of 1 Timestep per player
+            obs = env.reset()[0]
+            step = 0
+            episode = 1
+            episode_reward = 0
+            while (config['max_steps'] == 0 or step <= config['max_steps']) and (config['max_episodes'] == 0 or episode <= config['max_episodes']):
+                step += 1
+                state = preprocess_state(obs)
+                available_actions = dict(
+                    function=obs.observation['available_actions']
+                )
 
-            episode_reward += obs.reward
-            if obs.step_type is StepType.LAST:
-                terminal = True
-                print("Episode", episode, "finished. Score:", episode_reward)
-                episode_reward = 0
-                episode += 1
-            else:
-                terminal = False
+                episode_reward += obs.reward
+                if obs.step_type is StepType.LAST:
+                    terminal = True
+                    print("Episode", episode, "finished. Score:", episode_reward)
+                    episode_reward = 0
+                    episode += 1
+                else:
+                    terminal = False
 
-            if step > 1:
-                rl_agent.observe(terminal=terminal, reward=obs.reward)
+                if step > 1:
+                    rl_agent.observe(terminal=terminal, reward=obs.reward)
 
-            action = rl_agent.act(state, available_actions)
-            obs = env.step([get_action_function(obs, action)])[0]
+                action = rl_agent.act(state, available_actions)
+                action_for_sc = get_action_function(
+                    obs,
+                    action,
+                    config['env']['action_list']['function'],
+                    config['env']['screen_size']
+                )
+                # actions passed into env.step() are in a list with one action per player
+                obs = env.step([action_for_sc])[0]
+
+def main():
+    # load configuration
+    with open('config.json', 'r') as fp:
+        config = json.load(fp=fp)
 
 
 if __name__ == "__main__":
